1afe3ab2a0670505d670ff45b73792d4dcf49851
Mohammed Afzal <afzal@Mohammeds-MacBook-Air.local>
2018-03-17 17:17:14 +0530

    local library files first commit

---
+++ controllers/authorController.js
@@ -0,0 +1,110 @@
+var Author = require('../models/author');
+var Book = require('../models/book');
+var async = require('async');
+
+const {body, validationResult} = require('express-validator/check');
+const {sanitizeBody} = require('express-validator/filter');
+
+//Display list of all authors
+exports.author_list = function(req, res){
+	//res.send('NOT IMPLEMENTED: Author list');
+
+	Author.find()
+	.sort([['family_name', 'ascending']])
+	.exec(function(err, author_list){
+		if (err) return next(err);
+		else
+			res.render('author_list', {title: 'Author List', author_list: author_list});
+	});
+};
+
+//Display details of s specific author
+exports.author_detail = function(req, res, next){
+	//res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+
+	async.parallel({
+        thisauthor: function(callback){
+            Author.findById(req.params.id)
+            .exec(callback);
+        },
+        books: function(callback){
+            Book.find({'author': req.params.id})
+            .exec(callback);
+        }
+    },
+    function(err, results){
+        if(err) return next(err);
+        if(results.thisauthor == null){
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('author_detail', {title: 'Author Details', author: results.thisauthor, books: results.books});
+    });
+};
+
+//Display Author create form on GET
+exports.author_create_get = function(req, res, next){
+	//res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', {title: 'Create Author'});
+};
+
+//Handle Author create on POST
+exports.author_create_post = [
+    body('first_name').isLength({min:1}).trim().withMessage('First name should be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({min:1}).trim().withMessage('Family name should be specified.')
+    .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+    sanitizeBody('first_name').trim().escape(),
+    sanitizeBody('family_name').trim().escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+
+    (req, res, next) =>{
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            res.render('author_form', {title: 'Create Author', author: req.body, errors: errors.array()});
+        }
+        else{
+            var author = new Author(
+                    {
+                        first_name: req.body.first_name,
+                        family_name: req.body.family_name,
+                        date_of_birth: req.body.date_of_birth,
+                        date_of_death: req.body.date_of_death
+                    }
+                );
+
+            author.save(function(err){
+                if(err) return next(err);
+
+                res.redirect(author.url);
+            });
+        }
+    }
+];
+
+//Display Author delete form on GET
+exports.author_delete_get = function(req, res){
+	res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+//Handle Author delete on POST
+exports.author_delete_post = function(req, res){
+	res.send('NOT IMPLEMENTED: Author delete POST');
+};
+
+//Display Author update form on POST
+exports.author_update_get = function(req, res){
+	res.send('NOT IMPLEMENTED: Author update GET');
+};
+
+//Handle Author update on POST
+exports.author_update_post = function(req, res){
+	res.send('NOT IMPLEMENTED: Author update POST');
+};
---
+++ controllers/bookinstanceController.js
@@ -0,0 +1,101 @@
+var BookInstance = require('../models/bookinstance');
+var Book = require('../models/book');
+var async = require('async');
+
+const {body, validationResult} = require('express-validator/check');
+const {sanitizeBody} = require('express-validator/filter');
+
+//Display list of all bookinstances
+exports.bookinstance_list = function(req, res, next){
+	//res.send('NOT IMPLEMENTED: BookInstance list');
+
+	BookInstance.find({}, 'book imprint status due_back')
+	.populate('book')
+	//.sort([['status', 'ascending']])
+	.exec(function(err, list_instances){
+		if(err) return next(err);
+
+		else
+			res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_instances});
+	});
+};
+
+//Display details of a specific BookInstance
+exports.bookinstance_detail = function(req, res, next){
+	//res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+
+	BookInstance.findById(req.params.id)
+	.populate('book')
+	.exec(function(err, bookinstance){
+		if(err) return next(err);
+		res.render('bookinstance_detail', {title: 'Book Instance Detail', bookinstance: bookinstance})
+	});
+};
+
+//Display BookInstance create form on GET
+exports.bookinstance_create_get = function(req, res, next){
+	//res.send('NOT IMPLEMENTED: BookInstance create GET');
+
+	Book.find({}, 'title')
+	.exec(function(err, books) {
+		if(err) return next(err);
+
+		res.render('bookinstance_form', {title: 'Create Book Instance', books: books})
+	});
+};
+
+//Handle BookInstance create on POST
+exports.bookinstance_create_post = [
+	body('book', 'Book must be specified.').isLength({min: 1}).trim(),
+	body('imprint', 'Book imprint must be specified.').isLength({min: 1}).trim(),
+	body('due_back', 'Invalid due back date.').optional({checkFalsy: true}).isISO8601(),
+
+	sanitizeBody('*').trim().escape(),
+	sanitizeBody('due_back').toDate(),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+
+		var bookinstance = new BookInstance({
+			book: req.body.book,
+			imprint: req.body.imprint,
+			due_back: req.body.due_back,
+			status: req.body.status
+		});
+
+		if(!errors.isEmpty()){
+			Book.find({}, 'title')
+			.exec(function(err, books){
+				res.render('bookinstance_form', {title: 'Create Book Instance', books: books, bookinstance: bookinstance, errors: errors.array()});
+			});
+
+			return;
+		}
+		else{
+			bookinstance.save(function(err) {
+				if(err) return next(err);
+				res.redirect(bookinstance.url);
+			});
+		}
+	}
+];
+
+//Display BookInstance delete form on GET
+exports.bookinstance_delete_get = function(req, res){
+	res.send('NOT IMPLEMENTED: BookInstance delete GET');
+};
+
+//Handle BookInstance delete on POST
+exports.bookinstance_delete_post = function(req, res){
+	res.send('NOT IMPLEMENTED: BookInstance delete POST');
+};
+
+//Display BookInstance update form on POST
+exports.bookinstance_update_get = function(req, res){
+	res.send('NOT IMPLEMENTED: BookInstance update GET');
+};
+
+//Handle BookInstance update on POST
+exports.bookinstance_update_post = function(req, res){
+	res.send('NOT IMPLEMENTED: BookInstance update POST');
+};
